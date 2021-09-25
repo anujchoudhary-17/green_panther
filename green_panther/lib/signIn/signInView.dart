3634d1a149422c7a5588d3d13fd7336a298c6b25
Anuj Choudhary <50038492+anujchoudhary-17@users.noreply.github.com>
2021-09-25 10:50:19 -0400

    first push

---
+++ green_panther/lib/signIn/signInView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignInView extends StatefulWidget {
+  const SignInView({Key? key}) : super(key: key);
+
+  @override
+  _SignInViewState createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Hey'),
+      )
+    );
+  }
+}
